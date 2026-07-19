52caed5aeb8db725a60f2e46fbdfb4530db10970
houqi1 <145347890+houqi1@users.noreply.github.com>
2026-07-19 15:50:59 +0800

    Add PhoneixGlass materials with model normals and custom refraction shaders.
    Wire PixelNormalWS into PhoneixGlass offset/compose customs and SceneColor OffsetFraction, register /Project shader mapping in the game module, and enable Unreal MCP editor toolsets.

--- mp/Source/mp/mp.cpp
+++ mp/Source/mp/mp.cpp
@@ -1,6 +1,32 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "mp.h"
-#include "Modules/ModuleManager.h"
+#include "HAL/PlatformFileManager.h"
+#include "Misc/Paths.h"
+#include "ShaderCore.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, mp, "mp" );
+DEFINE_LOG_CATEGORY_STATIC(LogMpShaders, Log, All);
+
+void FmpModule::StartupModule()
+{
+	// Step 1: map project Shaders/ to virtual path for Material Custom #include
+	// Usage later: #include "/Project/YourShader.usf"
+	const FString ShaderDir = FPaths::ConvertRelativePathToFull(
+		FPaths::Combine(FPaths::ProjectDir(), TEXT("Shaders")));
+
+	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+	if (!PlatformFile.DirectoryExists(*ShaderDir))
+	{
+		PlatformFile.CreateDirectoryTree(*ShaderDir);
+	}
+
+	AddShaderSourceDirectoryMapping(TEXT("/Project"), ShaderDir);
+
+	UE_LOG(LogMpShaders, Log, TEXT("Shader source mapped: /Project -> %s"), *ShaderDir);
+}
+
+void FmpModule::ShutdownModule()
+{
+}
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FmpModule, mp, "mp");
--- mp/Source/mp/mp.h
+++ mp/Source/mp/mp.h
@@ -3,4 +3,14 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
 
+/**
+ * Game module: registers /Project -> <ProjectDir>/Shaders for Material Custom includes.
+ */
+class FmpModule : public IModuleInterface
+{
+public:
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
